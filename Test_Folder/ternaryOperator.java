4b6ef323276d0ba2c945d1d63201528e3eced296
naliahad <ahadali2842000@gmail.com>
2024-09-10 18:51:56 +0530

    Ternary operator

---
+++ Test_Folder/ternaryOperator.java
@@ -0,0 +1,16 @@
+package Test_Folder;
+
+public class ternaryOperator 
+{
+    public static void main(String[] args) 
+    {
+        
+        int larger =  (5>3)?5:3;
+        System.out.println(larger);
+
+        int num = 4;
+        String type = ((num%2)==0)? "even":"odd";
+        System.out.println(type);
+
+    } 
+}
