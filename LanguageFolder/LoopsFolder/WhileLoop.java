a1e20a7838590e94971a30af496e02e99a51233b
naliahad <ahadali2842000@gmail.com>
2024-09-10 19:27:07 +0530

    While Loop

---
+++ LanguageFolder/LoopsFolder/WhileLoop.java
@@ -0,0 +1,15 @@
+package LanguageFolder.LoopsFolder;
+
+public class WhileLoop 
+{
+    public static void main(String[] args) 
+    {
+        int counter = 0;
+
+        while (counter<100)
+        {
+            System.out.println(counter+1 + " Hello world");
+            counter++;
+        }
+    }
+}
