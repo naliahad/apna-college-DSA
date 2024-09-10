c4705030af36a24276fe468d6db8057024a3f8ed
naliahad <ahadali2842000@gmail.com>
2024-09-10 19:14:37 +0530

    Switch->Calculator

---
+++ LanguageFolder/Calculator.java
@@ -0,0 +1,42 @@
+package LanguageFolder;
+
+import java.util.Scanner;
+
+public class Calculator 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc  = new Scanner(System.in);
+
+        System.out.println("Enter 1st digit.");
+        int a = sc.nextInt();
+
+        System.out.println("Enter Operand.");
+        char operator = sc.next().charAt(0);
+
+        System.out.println("Enter 2nd digit.");
+        int b = sc.nextInt();
+
+        switch (operator)
+        {
+            case '+':System.out.println(a+b);
+            break;
+
+            case '-':System.out.println(a-b);
+            break;
+
+            case '/':System.out.println(a/b);
+            break;
+
+            case '*':System.out.println(a*b);
+            break;
+
+            case '%':System.out.println(a%b);
+            break;
+
+            default : System.out.println("Invalid input");
+        }
+
+        
+    }    
+}
