d69275eb361e194046b449edcff21974e558833d
naliahad <ahadali2842000@gmail.com>
2024-09-11 21:53:03 +0530

    Do while Loop

---
+++ LanguageFolder/LoopsFolder/doWhileLoop.java
@@ -0,0 +1,15 @@
+package LanguageFolder.LoopsFolder;
+
+public class doWhileLoop 
+{
+    public static void main(String[] args) 
+    {
+        int counter  =1;
+        do{
+            System.out.println(counter+": Hello World");
+            counter++;
+        }    
+
+        while(counter<=10);
+    }
+}
