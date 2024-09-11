23558b31cc086534688227f4c16bd1866d25ca0f
naliahad <ahadali2842000@gmail.com>
2024-09-11 21:48:32 +0530

    Revresed number 2

--- LanguageFolder/LoopsFolder/ReverseGivenNum.java
+++ LanguageFolder/LoopsFolder/ReverseGivenNum.java
@@ -1,9 +1,20 @@
 package LanguageFolder.LoopsFolder;
-
+import java.util.*;
 public class ReverseGivenNum 
 {
     public static void main(String[] args) 
     {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a integer.");
+        int num =  sc.nextInt();
+        int reversed = 0;
+        while (num!=0)
+        {
+            int lastDigit = num%10;
+            reversed = reversed*10 + lastDigit;
+            num = num/10;
+        }
+
+        System.out.println("Reversed: " + reversed);
     }    
 }
